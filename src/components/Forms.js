8e8ae979dfdcb76613fadbbf875db613618e7676
Aftab <patwekaraftab@gmail.com>
2021-03-03 15:31:15 +0530

    updated app.css and form.js

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,43 +1,57 @@
 import React from "react";
+import "../App.css";
 
-const Forms = ({inputText, setInputText, setTodos, todos, setStatus}) => {
+const Forms = ({ inputText, setInputText, setTodos, todos, setStatus }) => {
+  const inputHandler = (e) => {
+    setInputText(e.target.value);
+    // console.log(e.target.value);
+  };
 
-    const inputHandler =(e)=>{
-        setInputText(e.target.value);
-        // console.log(e.target.value);
-    }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
 
-    const submitHandler =(e)=>{
-        e.preventDefault();
-        setTodos([
-            ...todos, {text: inputText, completed: false, id: Math.random() * 1000}
-        ]);
-        setInputText("");
-
-    }
-
-    const statusHandler = (e) =>{
-        setStatus(e.target.value)
-    }
-
-
-
-
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
 
   return (
     <>
-      <form >
-        <input value={inputText} type="text" className="todo-input" onChange={inputHandler} />
-        <button onClick={submitHandler} className="todo-button" type="submit">
-          <i className="fas fa-plus-square"></i>
-        </button>
-        <div className="select">
-          <select onChange={statusHandler} name="todos" className="filter-todo">
-            <option value="all">All</option>
-            <option value="completed">Completed</option>
-            <option value="incompleted">Incompleted</option>
-          </select>
+      <form>
+          <div className="farm">
+          <div className="farm1">
+          <input
+            value={inputText}
+            type="text"
+            className="todo-input"
+            onChange={inputHandler}
+          />
+          <button onClick={submitHandler} className="todo-button" type="submit">
+            <i className="fas fa-plus-square"></i>
+          </button>
         </div>
+
+        <div  className="farm2">
+          <div className="select">
+            <select
+              onChange={statusHandler}
+              name="todos"
+              className="filter-todo"
+            >
+              <option value="all">All</option>
+              <option value="completed">Completed</option>
+              <option value="incompleted">Incompleted</option>
+            </select>
+          </div>
+        </div>
+              
+          </div>
+
       </form>
     </>
   );
